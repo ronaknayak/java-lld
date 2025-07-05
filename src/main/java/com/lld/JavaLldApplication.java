5201a2a45f67a0517a7e75ec00b8f719d83b2ade
Ronak Nayak <ronakanayak@gmail.com>
2025-07-05 17:44:07 +0530

    Initial Commit

---
+++ src/main/java/com/lld/JavaLldApplication.java
@@ -0,0 +1,13 @@
+package com.lld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLldApplication.class, args);
+	}
+
+}
